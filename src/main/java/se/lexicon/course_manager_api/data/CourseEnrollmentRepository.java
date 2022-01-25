bf6c22ee41480217a9f4b09144ad894060d67333
Erik Svensson <eosvensson@gmail.com>
2022-01-25 13:41:12 +0100

    Changed queries in CourseEnrollmentRepository to join fetch CourseGroup

--- src/main/java/se/lexicon/course_manager_api/data/CourseEnrollmentRepository.java
+++ src/main/java/se/lexicon/course_manager_api/data/CourseEnrollmentRepository.java
@@ -9,11 +9,19 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface CourseEnrollmentRepository extends JpaRepository<CourseEnrollment, String> {
-    List<CourseEnrollment> findByRegDate(LocalDate regDate);
 
-    @Query("SELECT c FROM CourseEnrollment c WHERE c.courseGroup.id = :id")
+    @Query("SELECT e FROM CourseEnrollment e INNER JOIN FETCH e.courseGroup g INNER JOIN FETCH g.course GROUP BY e")
+    List<CourseEnrollment> findAllCourseEnrollments();
+
+    @Query("SELECT e FROM CourseEnrollment e INNER JOIN FETCH  e.courseGroup g INNER JOIN FETCH g.course c WHERE UPPER(c.courseName) = :courseName")
+    List<CourseEnrollment> findByCourseName(@Param("courseName") String courseName);
+
+    @Query("SELECT e FROM CourseEnrollment e INNER JOIN FETCH e.courseGroup g INNER JOIN FETCH g.course WHERE e.regDate = :regDate")
+    List<CourseEnrollment> findByRegDate(@Param("regDate") LocalDate regDate);
+
+    @Query("SELECT e FROM CourseEnrollment e INNER JOIN FETCH e.courseGroup g INNER JOIN FETCH g.course WHERE e.courseGroup.id = :id")
     List<CourseEnrollment> findByCourseGroupId(@Param("id") String courseGroupId);
 
-    @Query("SELECT c FROM CourseEnrollment c WHERE c.student.id = :id")
+    @Query("SELECT e FROM CourseEnrollment e INNER JOIN FETCH e.courseGroup g INNER JOIN FETCH g.course WHERE e.student.id = :id")
     List<CourseEnrollment> findByStudentId(@Param("id") String studentId);
 }
