71795bbe9f96610870497477801c059f82a64a2f
Erik Svensson <eosvensson@gmail.com>
2022-01-25 13:16:11 +0100

    Changed queries in CourseGroupRepository to join fetch course

--- src/main/java/se/lexicon/course_manager_api/data/CourseGroupRepository.java
+++ src/main/java/se/lexicon/course_manager_api/data/CourseGroupRepository.java
@@ -9,21 +9,25 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface CourseGroupRepository extends JpaRepository<CourseGroup, String> {
-    @Query("SELECT c FROM CourseGroup c WHERE c.startDate > current_date")
+
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course")
+    List<CourseGroup> findAllCourseGroups();
+
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH  c.course WHERE c.startDate > current_date")
     List<CourseGroup> findNotYetStarted();
 
-    @Query("SELECT c FROM CourseGroup c WHERE c.startDate > current_date AND c.course.id = :id")
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course WHERE c.startDate > current_date AND c.course.id = :id")
     List<CourseGroup> findNotYetStarted(@Param("id") String courseId);
 
-    @Query("SELECT c FROM CourseGroup c WHERE current_date BETWEEN c.startDate AND c.endDate")
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course WHERE current_date BETWEEN c.startDate AND c.endDate")
     List<CourseGroup> findCurrentCourseGroups();
 
-    @Query("SELECT c FROM CourseGroup c WHERE :date BETWEEN c.startDate AND c.endDate")
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course WHERE :date BETWEEN c.startDate AND c.endDate")
     List<CourseGroup> findCourseGroupsBy(@Param("date") LocalDate date);
 
-    @Query("SELECT c FROM CourseGroup c WHERE :date > c.endDate")
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course WHERE :date > c.endDate")
     List<CourseGroup> findByEndDateAfter(@Param("date") LocalDate date);
 
-    @Query("SELECT c FROM CourseGroup c WHERE c.course.id = :id")
+    @Query("SELECT c FROM CourseGroup c INNER JOIN FETCH c.course WHERE c.course.id = :id")
     List<CourseGroup> findAllByCourseId(@Param("id") String courseId);
 }
