2171fe2d25f7e2a59489e706308a8097cee569b6
Erik Svensson <eosvensson@gmail.com>
2022-01-25 14:25:03 +0100

    Changed queries in StudentRepository to join fetch CourseGroup

--- src/main/java/se/lexicon/course_manager_api/data/StudentRepository.java
+++ src/main/java/se/lexicon/course_manager_api/data/StudentRepository.java
@@ -9,8 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
-    @Query("SELECT s FROM Student s WHERE UPPER(s.email) = UPPER(:email)")
+    @Query("SELECT s FROM Student s INNER JOIN FETCH s.courses c INNER JOIN FETCH c.courseGroup g INNER JOIN FETCH g.course WHERE UPPER(s.email) = UPPER(:email)")
     Optional<Student> findByEmail(@Param("email") String email);
+
+    @Query("SELECT s FROM Student s INNER JOIN FETCH s.courses c INNER JOIN FETCH c.courseGroup g INNER JOIN FETCH g.course WHERE s.id = :id")
+    Optional<Student> findStudentById(@Param("id") String id);
 
     @Query("SELECT s FROM Student s WHERE UPPER(CONCAT(s.firstName, ' ', s.lastName)) LIKE UPPER(CONCAT('%', :name, '%'))")
     List<Student> findByName(@Param("name") String name);
